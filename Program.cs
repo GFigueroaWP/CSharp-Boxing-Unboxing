8e6cff15a3fa57a6a68dd01387defbd9acc2e6db
Guido Figueroa <guido.figueroa.dev@gmail.com>
2023-10-03 11:42:33 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Crea una aplicación de consola y sigue las siguientes instrucciones para descomprimir de forma segura una matriz de Objetos. ​
+//Create an empty List of type object
+List<object> box = new List<object>();
+//Add the following values to the list: 7, 28, -1, true, "chair"
+box.Add(7);
+box.Add(28);
+box.Add(-1);
+box.Add(true);
+box.Add("chair");
+//Loop through the list and print all values
+foreach (var item in box)
+{
+    Console.WriteLine(item);
+}
+//Add all values that are Int type together and output the sum
+int sum = 0;
+foreach (var item in box)
+{
+    if (item is int)
+    {
+        sum += (int)item;
+    }
+}
+Console.WriteLine($"La suma de los enteros es: {sum}");
